3cc0c15a7b5c4d5ce6e353ea2ead4fc8497cb212
Saddam <msaddamh007@gmail.com>
2023-09-17 02:19:26 +0600

    post API from Postman to nodejs

--- api.js
+++ api.js
@@ -2,11 +2,20 @@ const express = require('express');
 const dbConnect = require('./mongodb');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', async (req, res) => {
     let data = await dbConnect();
     data = await data.find().toArray();
     console.log(data);
     res.send(data);
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.send({
+        name:'Saddam'
+    });
 });
 
 app.listen(5000);
