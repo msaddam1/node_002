203720fd8c4a1bacdd8534455bf4f07d7d8d629f
Saddam <msaddamh007@gmail.com>
2023-09-16 23:04:28 +0600

    db new file

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://127.0.0.1:27017';
-const database = 'e-comm'
-const client = new MongoClient(url);
-
-async function dbConnect() {
-    let result = await client.connect();
-    let db = result.db(database);
-    return db.collection('products');
-    // let data= await collection.find({name:'galaxy'}).toArray();
-    // console.log(response);
-};
+const dbConnect= require('./mongodb');
 // dbConnect().then((resp) => {
 //     resp.find({}).toArray().then((data) => {
 //         console.warn(data);
@@ -19,7 +8,7 @@ async function dbConnect() {
 
 const main = async()=>{
     let data =await dbConnect();
-    data =await data.find().toArray();
+    data =await  data.find().toArray();
     console.warn(data);
 }
 main();
