012e1d8203a684d8dd126523a52f8c8537ff52fe
Saddam <msaddamh007@gmail.com>
2023-09-17 01:00:49 +0600

    many delete from nodejs

--- delete.js
+++ delete.js
@@ -2,7 +2,7 @@ const dbConnect= require('./mongodb');
 
 const deleteData = async ()=>{
     let data = await dbConnect();
-    let result = await data.deleteOne({name:'max 1'});
+    let result = await data.deleteMany({name:'note 5'});
     console.warn(result);
 };
 deleteData();
