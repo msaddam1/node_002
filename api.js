9f7d278875aa95e2739a4c614e6b43458c6b3420
Saddam <msaddamh007@gmail.com>
2023-09-17 02:27:54 +0600

    insert data from Postman to Database

--- api.js
+++ api.js
@@ -11,11 +11,10 @@ app.get('/', async (req, res) => {
     res.send(data);
 });
 
-app.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send({
-        name:'Saddam'
-    });
+app.post('/',async (req,res)=>{
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body)
+    res.send(result);
 });
 
 app.listen(5000);
