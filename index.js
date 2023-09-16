6547beaa5b05ce724f600d0f20b88d2d1e8025ee
Saddam <msaddamh007@gmail.com>
2023-09-16 22:31:52 +0600

    promise handle

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const {MongoClient}= require('mongodb');
-const url='mongodb://127.0.0.1:27017';
-const database='e-comm'
-const client=new MongoClient(url);
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
+const database = 'e-comm'
+const client = new MongoClient(url);
 
-async function dbConnect()
-{
-    let result=await client.connect();
-    let db=result.db(database);
+async function dbConnect() {
+    let result = await client.connect();
+    let db = result.db(database);
     return db.collection('products');
     // let data= await collection.find({name:'galaxy'}).toArray();
     // console.log(response);
 };
-dbConnect().then((resp)=>{
-    resp.find({}).toArray().then((data)=>{
-        console.warn(data);
+// dbConnect().then((resp) => {
+//     resp.find({}).toArray().then((data) => {
+//         console.warn(data);
 
-    })
-});
+//     })
+// });
+
+const main = async()=>{
+    let data =await dbConnect();
+    data =await data.find().toArray();
+    console.warn(data);
+}
+main();
